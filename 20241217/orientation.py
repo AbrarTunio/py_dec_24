3fb95bff1557840c11f2c37825fe8e404421ef67
AbrarTunio <abrar.tunio@yahoo.com>
2024-12-19 17:18:21 +0500

    starting python

---
+++ 20241217/orientation.py
@@ -0,0 +1,14 @@
+
+# 1: Installing Python
+# 3: Variables, expressions and statements
+# 4: Conditional Execution
+# 5: Functions
+# 6: Loops and Iterations
+# 7: Strings
+# 8: Files
+# 9: Lists
+# 10: Dictionaries
+# 11: Tuples
+# 13: Network Programming
+# 14: Using Web Services
+
---
+++ 20241218/first.py
@@ -0,0 +1,9 @@
+# taking out output
+
+print("Hello WOrld!")
+
+# creating variables and printing those out.
+myName = "Abrar Hussain"
+age = 55
+
+print(myName,"is",age,"years old")
