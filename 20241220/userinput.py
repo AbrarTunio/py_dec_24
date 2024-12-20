9aaf8b5d5de4c7eb3aebc2e69c95b2ce9bd68af0
AbrarTunio <abrar.tunio@yahoo.com>
2024-12-20 17:52:36 +0500

    taking user input

---
+++ 20241220/userinput.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the value of num1")
+num2 = input("Enter the value of num2")
+
+print( "The type of num1:" , type(num1) )
+print( "The type of num2:" , type(num2) )
+
+num1 =  int( num1 )
+num2 =  int( num2 )
+result =  num1 + num2 
+
+print("------------------")
+print( "The type of num1:" , type(num1) )
+print( "The type of num2:" , type(num2) )
+
+print(result)
