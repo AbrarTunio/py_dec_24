928b47df906410a68e3bdd921dfab5f7545594fe
AbrarTunio <abrar.tunio@yahoo.com>
2024-12-20 15:39:39 +0500

    lecture 3 statements

---
+++ 20241219/variables.py
@@ -0,0 +1,35 @@
+studentName = "Safia"
+# decRegistration = "1"
+# decTutionFee = "4"
+# janTutionFee = "3"
+# febTutionFee = "3"
+
+# tAmount = decRegistration + decTutionFee + janTutionFee + febTutionFee 
+# print( studentName, "has paid", tAmount)
+
+# print( studentName*5 )
+
+num1 = 5
+num2 = 6
+
+# result = num1 / num2
+# result = num1 % num2
+# result = num1 // num2
+
+result = num1 ** num2
+print( result )
+
+# Precedence
+
+# Brackets - ()
+# Power - **
+# Division - /
+# Multi - *
+# Addition/Subtraction +/-
+
+c = 20
+f = 9/3 * (32 - c)
+
+print( 'The value of f is' , f )
+
+
