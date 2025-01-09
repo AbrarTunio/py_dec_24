893c65a44c96362767d32638413398a5a306a446
AbrarTunio <abrar.tunio@yahoo.com>
2025-01-09 18:10:38 +0500

    3 days work

---
+++ 20250109/break.py
@@ -0,0 +1,10 @@
+# value = True
+while (True):
+    name = input("Enter your name: ")
+    if  name == "don" :
+        # value = False
+        break
+    else:
+        print(name , "is an awesome name")
+
+print("Program Broken")
---
+++ 20250109/list.py
@@ -0,0 +1,30 @@
+fruits = ['tomato' , 'apple' , 'potato']
+fruits[2] = 'kiwi'
+
+# print( fruits)
+
+
+# f = "banana"
+# f[0] = "m"
+# f_n = f.replace("b" , 'm')
+# f = f.replace("b" , 'm')
+# print(f)
+# print(f_n)
+
+
+
+names = ['abrar' , 'israr' , 'iqrar']
+surnames = ['tunio' , 'qureshi' , 'memon']
+
+# combined = names + surnames
+# print(combined)
+# i = 0
+# for name in names:
+#     print( name + surnames[i] )
+#     i = i + 1
+
+# i = 0
+# while( len( names ) > i ):
+#     print( names[ i ] + surnames[ i ] )
+#     i += 1 
+
